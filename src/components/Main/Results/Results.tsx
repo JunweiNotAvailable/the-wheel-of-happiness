3894d8a9b224041c819477522a732dd4a43c1cba
Junwei <wertycxx@gmail.com>
2023-12-24 11:37:36 +0800

    added animals description

--- src/components/Main/Results/Results.tsx
+++ src/components/Main/Results/Results.tsx
@@ -1,14 +1,74 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Results.css';
 import { MainProps } from '../Main'
-import { names } from '../../../utils/Constants';
+import { animals, colors, names } from '../../../utils/Constants';
+import { useTranslation } from 'react-i18next';
+import { PolarArea } from 'react-chartjs-2';
+import { Chart, ChartOptions, RadialLinearScale, ArcElement } from 'chart.js'
+
+Chart.register(RadialLinearScale);
+Chart.register(ArcElement);
 
 const Results: React.FC<MainProps> = ( props ) => {
+
+  const { t } = useTranslation();
+  // chart config
+  const scores = names.map(n => props.score[n as keyof typeof props.score]);
+  const chartData = {
+    labels: names.map(n => t(`title_${(n)}`)),
+    datasets: [
+      {
+        data: scores,
+        backgroundColor: colors.map(c => c + '88'),
+        hoverBackgroundColor: colors.map(c => c + 'aa'),
+        borderColor: '#f7f4ee',
+        borderWidth: 1.5,
+      },
+    ],
+  };
+  const chartOptions: ChartOptions<'polarArea'> = {
+    animation: {
+      animateRotate: true,
+      animateScale: true
+    },
+    scales: {
+      r: {
+        ticks: {
+          display: false,
+          stepSize: 1,
+        },
+        grid: {
+          color: '#ccc'
+        },
+        pointLabels: {
+          display: true,
+          centerPointLabels: true,
+          font: {
+            size: 9
+          },
+        },
+      }
+    },
+  };
+  const [average, setAverage] = useState<number[]>([-1, -1, -1]);
+  const [animal, setAnimal] = useState<{ type: string, score: number[], img: string }>({ type: '', score: [], img: '' });
+
+  useEffect(() => {
+    // calculate the score
+    const averageScore = [
+      Math.round(scores.slice(0, 3).map(s => s <= 3 ? 0 : 1).reduce((sum: number, v: number) => sum + v, 0) / 3),
+      Math.round(scores.slice(3, 6).map(s => s <= 3 ? 0 : 1).reduce((sum: number, v: number) => sum + v, 0) / 3),
+      Math.round(scores.slice(6, 9).map(s => s <= 3 ? 0 : 1).reduce((sum: number, v: number) => sum + v, 0) / 3),
+    ];
+    setAverage(averageScore);
+    setAnimal(animals.find(a => a.score.filter((s, i) => s === averageScore[i]).length === 3) || { type: '', score: [], img: '' });
+  }, []);
+
   return (
     <div className='app results absolute' style={{ overflow: 'auto' }}>
       
       {/* main components */}
-      <div className='results-screen absolute' style={{ top: 0, left: 0, width: '100%', height: '100%', background: '#ffffffdd' }}>
+      <div className='results-screen absolute' style={{ top: 0, left: 0, width: '100%', height: '100%' }}>
         {/* top section */}
         <div className='top-section flex align-center'>
           <i className='fa-solid fa-bars'/>
@@ -16,15 +76,15 @@ const Results: React.FC<MainProps> = ( props ) => {
         </div>
         {/* pie chart */}
         <div className='center' style={{ marginTop: 24 }}>
-          <figure className='wheel'>
-            {names.map((name, i) => {
-              return (
-                <circle></circle>
-              )
-            })}
-          </figure>
+          <div className='wheel-container'>
+            <div style={{ textAlign: 'center', fontWeight: 600 }}>The Wheel of Happiness</div>
+            <PolarArea data={chartData} title='The Wheel of Happiness' options={chartOptions}/>
+          </div>
         </div>
-
+        {/* animal */}
+        <div style={{ fontSize: 18, fontWeight: 600, textAlign: 'center' }}><span style={{ fontWeight: 400, fontSize: 16 }}>你是</span> {t(animal.type)}</div>
+        <img className='animal-img' alt='' style={{ margin: '8px 0' }} src={`./images/${animal.img}`}/>
+        <div style={{ margin: '12px 24px', textAlign: 'center' }}>{t(`${animal.type}_description`)}</div>
       </div>
 
     </div>
--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -2,17 +2,18 @@ import { Score } from "./Interfaces";
 
 export const initialScore: Score = { body: 0, mind: 0, soul: 0, family: 0, romance: 0, friends: 0, mission: 0, money: 0, growth: 0 };
 export const names = ['body', 'mind', 'soul', 'family', 'romance', 'friends', 'mission', 'money', 'growth'];
+export const colors = ['#eed056', '#ec1c24', '#7be7ba', '#e0a21b', '#ed8fad', '#3c42f4', '#0ed145', '#00a8f3', '#afa7dc'];
 export const wheelOfLifeName = 'wheel-of-life-name';
 export const wheelOfLifeScore = 'wheel-of-life-score';
 export const wheelOfLifePage = 'wheel-of-life-page';
 
 export const animals = [
-  { type: 'snail', score: [0, 0, 0], img: '' },
-  { type: 'beaver', score: [0, 0, 1], img: '' },
-  { type: 'firefly', score: [0, 1, 0], img: '' },
-  { type: 'ant', score: [0, 1, 1], img: '' },
-  { type: 'antelope', score: [1, 0, 0], img: '' },
-  { type: 'eagle', score: [1, 0, 1], img: '' },
-  { type: 'dolphin', score: [1, 1, 0], img: '' },
-  { type: 'lion', score: [1, 1, 1], img: '' },
+  { type: 'snail', score: [0, 0, 0], img: 'snail.png' },
+  { type: 'beaver', score: [0, 0, 1], img: 'beaver.png' },
+  { type: 'firefly', score: [0, 1, 0], img: 'firefly.png' },
+  { type: 'ant', score: [0, 1, 1], img: 'ant.png' },
+  { type: 'antelope', score: [1, 0, 0], img: 'antelope.png' },
+  { type: 'eagle', score: [1, 0, 1], img: 'eagle.png' },
+  { type: 'dolphin', score: [1, 1, 0], img: 'dolphin.png' },
+  { type: 'lion', score: [1, 1, 1], img: 'lion.png' },
 ];
